bcc8948be3dbd045c04ce5a755b4c8d9aacdbb1b
ronnenn podolsky <podolsky.ronnen@gmail.com>
2023-01-07 20:23:48 +0200

    initial rest api for goals with mongo connected

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,12 @@
+import Goal from '../models/goalModel.js'
 import asyncHandler from 'express-async-handler'
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'get g' })
+    const goals = await Goal.find()
+    res.status(200).json({ goals })
 })
 
 // @desc    Set goals
@@ -15,21 +17,38 @@ const setGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('please send a text field')
     }
-    res.status(201).json({ message: `${req.body.text}`})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(201).json(goal)
 })
 
 // @desc    update goals
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `update goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+    if (!goal){
+        res.status(400)
+        throw new Error('goal not found')
+
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.status(200).json(updatedGoal)
 })
 
 // @desc    delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `delete goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+    if (!goal){
+        res.status(400)
+        throw new Error('goal not found')
+    }
+    await goal.remove()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
+import colors from 'colors'
 import dotenv from 'dotenv';
 import express from "express";
 import { errorHandler } from './middleware/errorMiddleware.js';
 import { goalsRouter } from "./routes/goalRoutes.js";
 dotenv.config()
+
+import { connectDB } from './config/db.js';
+connectDB()
 const port = process.env.PORT || 5004;
+
 
 const app = express();
 app.use(express.urlencoded({ extended: false }))
